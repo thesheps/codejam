b3dc00038137bee4df984ec63853eeafd8d62028
Chris Shepherd <thesheps@gmail.com>
2016-07-10 21:38:24 +0100

    Problem 1

---
+++ CodeJam/Controllers/ProblemController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace CodeJam.Controllers
+{
+    public class ProblemController : Controller
+    {
+        public ActionResult One()
+        {
+            return View();
+        }
+    }
+}
---
+++ CodeJam/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeJam.Startup))]
+namespace CodeJam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
