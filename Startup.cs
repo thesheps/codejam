963db368a420aaab2537215b766219d78126b6ca
Chris Shepherd <thesheps@gmail.com>
2016-07-10 18:31:01 +0100

    Fix

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,7 @@ namespace CodeJam
         {
             app.Run(context =>
             {
-                return context.Response.WriteAsync("Hello from ASP.NET Core!");
+                return context.Response.WriteAsync("Hello from Azure!");
             });
         }
     }
